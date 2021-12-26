8a5842bb4a79f56833a842d27e40254a90a9ea4e
Adler Weber <adlerweber8430@gmail.com>
2021-12-25 23:35:55 -0500

    Implement get_video_url and remove API comments

--- backend/src/app.py
+++ backend/src/app.py
@@ -11,7 +11,6 @@ from flask import Flask
 from flask import request
 from werkzeug.utils import secure_filename
 
-from media import *
 import media
 
 from db import User
@@ -28,22 +27,18 @@ app = Flask(__name__)
 load_dotenv()
 
 # constants
-ENV = "prod"
+ENV = "dev"
 DB_NAME = str(os.environ.get("DB_NAME")).strip()
 DB_USERNAME = str(os.environ.get("DB_USERNAME")).strip()
 DB_PASSWORD = str(os.environ.get("DB_PASSWORD")).strip()
 G_CLIENT_ID = str(os.environ.get("G_CLIENT_ID")).strip()
-AWS_ACCESS_KEY_ID = str(os.environ.get("ACCESS_KEY_ID")).strip()
-AWS_SECRET_ACCESS_KEY = str(os.environ.get("SECRET_ACCESS_KEY")).strip()
-AWS_BUCKET_NAME = 'appdev-backend-final'
-AWS_BUCKET_REGION_NAME = 'us-east-2'
+AWS_ACCESS_KEY_ID = str(os.environ.get("AWS_ACCESS_KEY_ID")).strip()
+AWS_SECRET_ACCESS_KEY = str(os.environ.get("AWS_SECRET_ACCESS_KEY")).strip()
+CF_PUBLIC_KEY_ID = str(os.environ.get("CF_PUBLIC_KEY_ID")).strip()
+CF_PRIVATE_KEY_FILE = './private_key.pem'
 
-# global Amazon S3 client
-s3 = boto3.client('s3', region_name=AWS_BUCKET_REGION_NAME, 
-                  endpoint_url=f'https://s3.{AWS_BUCKET_REGION_NAME}.amazonaws.com',
-                  aws_access_key_id=AWS_ACCESS_KEY_ID, 
-                  aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-                  config=Config(signature_version='s3v4'))
+# global AWS instance TODO: check if multiple Flask clients share this instance
+aws = media.AWS(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, CF_PUBLIC_KEY_ID, CF_PRIVATE_KEY_FILE)
 
 # To use on your local machine, you must configure postgres at port 5432 and put your credentials in your .env.
 if ENV == "dev":
@@ -54,13 +49,11 @@ else:
     app.config["SQLALCHEMY_ECHO"] = False
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 512 # Don't accept files larger than 0.5GB
-app.config['UPLOAD_EXTENSIONS'] = ['.mp4']
-app.config['UPLOAD_PATH'] = '/tmp'
 
 db.init_app(app)
 with app.app_context():
     db.create_all()
+
 
 # Routes
 def success_response(data, code=200):
@@ -73,34 +66,6 @@ def failure_response(message, code=404):
 
 @app.route("/api/user/authenticate/", methods=["POST"])
 def authenticate_user():
-    """
-    Request:
-    {
-        "token": "{User's ID Token}"
-    }
-    Response:
-    {
-        "uid": "{User's local id}",
-        "display_name": "{User's display name}",
-        "email": "{User's email}"
-        "uploads":
-            [
-                {
-                    "vid": "{video id}",
-                    "display_title": "{display title of video}"
-                    "tags": [
-                        {
-                            "tid": 1,
-                            "name": "backhand"
-                        }
-                        ...
-                    ]
-                }
-                ...
-            ]
-    }
-    """
-
     body = json.loads(request.data)
     token = body.get("token")
 
@@ -132,25 +97,7 @@ def authenticate_user():
 
 
 @app.route("/api/user/<int:uid>/media/")
-def get_user_media(uid):
-    """
-    Request:
-    Response:
-    {
-        "uploads": [
-            {
-                "vid": 1,
-                "display_title": "{the new display title for the video}",
-                "tags": [
-                    {
-                        "tid": 1,
-                        "name": "backhand"
-                    }
-                ]
-            }
-        ]
-    }
-    """
+def get_all_user_media(uid):
     user = User.query.filter_by(uid=uid).first()
     if user is None:
         return failure_response("User not found.")
@@ -161,24 +108,6 @@ def get_user_media(uid):
 
 @app.route("/api/media/<int:vid>/update-title/", methods=['POST'])
 def update_upload_title(vid):
-    """
-    Request:
-    {
-        "new_title": "{the new display title for the video}" or None (keeps display title the same)
-    }
-    Response:
-    {
-        "vid": {video id},
-        "display_title": {display title of video}
-        "tags": [
-            {
-                "tid": 1,
-                "name": "backhand"
-            }
-        ]
-    }
-    """
-
     upload = Upload.query.filter_by(vid=vid).first()
 
     if upload is None:
@@ -198,44 +127,23 @@ def update_upload_title(vid):
 
 @app.route("/api/media/<int:vid>/")
 def get_video_url(vid):
-    """
-    Request: NA
-    Response:
-    {
-        "url": "www.something.m3u8",
-    }
-    """
     upload = Upload.query.filter_by(vid=vid).first()
 
     if upload is None:
         return failure_response("Could not locate video id in database.")
 
     vkey = upload.vkey
-    object_url = get_object_url(AWS_BUCKET_NAME, AWS_BUCKET_REGION_NAME, vkey)
+    expire_date = datetime.datetime.utcnow() + datetime.timedelta(hours=3)
+    url = aws.get_presigned_hls_url(vkey, expire_date)
 
-    if object_url is None:
-        return failure_response("Could not locate video in S3 bucket.")
+    if url is None:
+        return failure_response("An error occurred when presigning the HLS URL.")
 
-    return success_response({'url': object_url})
+    return success_response({'url': url})
 
 
 @app.route("/api/media/", methods=["POST"])
 def upload_video():
-    """
-    Request encoding = form-data:
-    {
-        'file': <FILEDATA>
-        'filename': 'backhand.mp4',
-        'display_title': 'Backhand Serve 12-2-2021',
-        'uid': 5
-    }
-    Response encoding = JSON:
-    {
-        'vid': 4,
-        'url': 'www.something.m3u8'
-    }
-    """
-
     filename = request.form.get("filename")
     display_title = request.form.get("display_title")
     uid = request.form.get("uid")
@@ -306,23 +214,6 @@ def upload_callback():
 
 @app.route("/api/media/<int:vid>/tag/", methods=['POST'])
 def add_tag(vid):
-    """
-    Request:
-    {
-        "name": "backhand"
-    }
-    Response:
-    {
-        "vid": 1,
-        "display_title": "{the new display title for the video}",
-        "tags": [
-            {
-                "tid": 1,
-                "name": "backhand"
-            }
-        ]
-    }
-    """
     upload = Upload.query.filter_by(vid=vid).first()
     if upload is None:
         return failure_response("Upload not found.")
@@ -351,18 +242,6 @@ def add_tag(vid):
 
 @app.route("/api/media/<int:vid>/tag/")
 def get_tags(vid):
-    """
-    Request:
-    Response:
-    {
-        "tags": [
-            {
-                "tid": 1,
-                "name": "backhand"
-            }
-        ]
-    }
-    """
     upload = Upload.query.filter_by(vid=vid).first()
     if upload is None:
         return failure_response("Upload not found.")
@@ -372,15 +251,6 @@ def get_tags(vid):
 
 @app.route("/api/media/<int:vid>/tag/<int:tid>/", methods=['DELETE'])
 def delete_tag(vid, tid):
-    """
-    Request:
-    Response:
-    {
-        "vid": 1,
-        "display_title": "{the display title for the video}",
-        "tags": []
-    }
-    """
     upload = Upload.query.filter_by(vid=vid).first()
     if upload is None:
         return failure_response("Upload not found.")
