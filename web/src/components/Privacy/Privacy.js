d1f520d1f95bfe68e62f5471e88674b7456152e7
Adler Weber <adlerweber8430@gmail.com>
2022-06-10 19:54:00 -0400

    Add privacy policy and footer

---
+++ web/src/components/Privacy/Privacy.js
@@ -0,0 +1,62 @@
+// test
+export default function Privacy() {
+
+    return (
+        <div>
+            <h1>Privacy Policy</h1>
+            <p>Protecting your private information is our priority. This Statement of Privacy applies to MyAce, and MyAce.ai LLC and governs data collection and usage. For the purposes of this Privacy Policy, unless otherwise noted, all references to MyAce.ai LLC include MyAce.ai and MyAce. The MyAce application is a Sports and Coaching application. By using the MyAce application, you consent to the data practices described in this statement.</p>
+            <h2>Collection of your Personal Information</h2>
+            <p>
+            MyAce may collect anonymous demographic information, which is not unique to you, such as your:
+            <ul>
+                <li>Age</li>
+                <li>Gender</li>
+            </ul>
+            We do not collect any personal information about you unless you voluntarily provide it to us. However, you may be required to provide certain personal information to us when you elect to use certain products or services. These may include: (a) registering for an account; (b) entering a sweepstakes or contest sponsored by us or one of our partners; (c) signing up for special offers from selected third parties; (d) sending us an email message; (e) submitting your credit card or other payment information when ordering and purchasing products and services. To wit, we will use your information for, but not limited to, communicating with you in relation to services and/or products you have requested from us. We also may gather additional personal or non-personal information in the future.
+            </p>
+            <h2>Sharing Information with Third Parties</h2>
+            <p>MyAce does not sell, rent or lease its customer lists to third parties.</p>
+            <p>MyAce may share data with trusted partners to help perform statistical analysis, send you email or postal mail, provide customer support, or arrange for deliveries. All such third parties are prohibited from using your personal information except to provide these services to MyAce, and they are required to maintain the confidentiality of your information.</p>
+            <p>MyAce may disclose your personal information, without notice, if required to do so by law or in the good faith belief that such action is necessary to: (a) conform to the edicts of the law or comply with legal process served on MyAce or the site; (b) protect and defend the rights or property of MyAce; and/or (c) act under exigent circumstances to protect the personal safety of users of MyAce, or the public.</p>
+            <h2>Right to Deletion</h2>
+            <p>
+            Subject to certain exceptions set out below, on receipt of a verifiable request from you, we will:
+            <ul>
+                <li>Delete your personal information from our records; and</li>
+                <li>Direct any service providers to delete your personal information from their records.</li>
+            </ul>
+            Please note that we may not be able to comply with requests to delete your personal information if it is necessary to:
+            <ul>
+                <li>Complete the transaction for which the personal information was collected, fulfill the terms of a written warranty or product recall conducted in accordance with federal law, provide a good or service requested by you, or reasonably anticipated within the context of our ongoing business relationship with you, or otherwise perform a contract between you and us;</li>
+                <li>Detect security incidents, protect against malicious, deceptive, fraudulent, or illegal activity; or prosecute those responsible for that activity;</li>
+                <li>Debug to identify and repair errors that impair existing intended functionality;</li>
+                <li>Exercise free speech, ensure the right of another consumer to exercise his or her right of free speech, or exercise another right provided for by law;</li>
+                <li>Comply with the California Electronic Communications Privacy Act;</li>
+                <li>Engage in public or peer-reviewed scientific, historical, or statistical research in the public interest that adheres to all other applicable ethics and privacy laws, when our deletion of the information is likely to render impossible or seriously impair the achievement of such research, provided we have obtained your informed consent;</li>
+                <li>Enable solely internal uses that are reasonably aligned with your expectations based on your relationship with us</li>
+                <li>Comply with an existing legal obligation; or</li>
+                <li>Otherwise use your personal information, internally, in a lawful manner that is compatible with the context in which you provided the information.</li>
+            </ul></p>
+            <h2>Children Under Thirteen</h2>
+            <p>MyAce does not knowingly collect personally identifiable information from children under the age of thirteen. If you are under the age of thirteen, you must ask your parent or guardian for permission to use this application.</p>
+            <h2>Email Communications</h2>
+            <p>From time to time, MyAce may contact you via email for the purpose of providing announcements, promotional offers, alerts, confirmations, surveys, and/or other general communication. In order to improve our Services, we may receive a notification when you open an email from MyAce or click on a link therein.</p>
+            <h2>External Data Storage Sites</h2>
+            <p>We may store your data on servers provided by third party hosting vendors with whom we have contracted.</p>
+            <h2>Changes to this Statement</h2>
+            <p>MyAce reserves the right to change this Privacy Policy from time to time. We will notify you about significant changes in the way we treat personal information by sending a notice to the primary email address specified in your account, by placing a prominent notice on our application, and/or by updating any privacy information. Your continued use of the application and/or Services available after such modifications will constitute your: (a) acknowledgment of the modified Privacy Policy; and (b) agreement to abide and be bound by that Policy.</p>
+            <h2>Contact Information</h2>
+            <p>MyAce welcomes your questions or comments regarding this Statement of Privacy. If you believe that MyAce has not adhered to this Statement, please contact MyAce at:</p>
+            <br/>
+            <p>MyAce.ai LLC
+               8 The Green Suite, A
+               Dover, Delaware 19901</p>
+            <br/>
+            <p>Email address: MyAceai@gmail.com</p>
+            <br/>
+            <p>Telephone number: 8324441653</p>
+            <br/><br/>
+            <p>Effective as of June 04, 2022</p>
+        </div>
+    )
+}
