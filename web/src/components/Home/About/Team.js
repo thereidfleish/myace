ab443a76bde6c61dcaa11e99e276687f6b635fb3
Adler Weber <adlerweber8430@gmail.com>
2022-06-11 23:18:11 -0400

    Yeesh 😶

--- web/src/components/Home/About/Team.js
+++ web/src/components/Home/About/Team.js
@@ -14,12 +14,12 @@ export default function Team() {
       picture: "https://media-exp1.licdn.com/dms/image/C4E03AQF4q69YbpwsSQ/profile-displayphoto-shrink_200_200/0/1623430072214?e=1657756800&v=beta&t=EhJ8bCDvOiuO58y9W6k8ulCaIOy1dnvQxkNgOFrZDSc",
       linkedin: 'https://www.linkedin.com/in/adler-weber-106a651ab/'
     },
-    {
-      name: 'Grant Rinehimer',
-      description: 'Backend',
-      picture: "https://media.superhuman.com/images/_/https%3A%2F%2Flh6.googleusercontent.com%2Fr66PDijtSugTK_jQCYLXD_OGwI-OJW_oZRYlBPczQqMaWa0Pdh3vZHthGJJ1blwxp_JGxxzc1xs1KlUENMSoW4hlUIdyFkUue39t81i09cSobY3ZY8PnjT6DMFOrS2C7Mif9wYoCm0bh",
-      linkedin: 'https://www.linkedin.com'
-    },
+    // {
+    //   name: 'Grant Rinehimer',
+    //   description: 'Backend',
+    //   picture: "https://media.superhuman.com/images/_/https%3A%2F%2Flh6.googleusercontent.com%2Fr66PDijtSugTK_jQCYLXD_OGwI-OJW_oZRYlBPczQqMaWa0Pdh3vZHthGJJ1blwxp_JGxxzc1xs1KlUENMSoW4hlUIdyFkUue39t81i09cSobY3ZY8PnjT6DMFOrS2C7Mif9wYoCm0bh",
+    //   linkedin: 'https://www.linkedin.com'
+    // },
     {
       name: 'Reid Fleishman',
       description: 'iOS',
@@ -32,12 +32,12 @@ export default function Team() {
       picture: "https://media-exp1.licdn.com/dms/image/C4E03AQGkzME7hPJWvw/profile-displayphoto-shrink_200_200/0/1608691026962?e=1657756800&v=beta&t=xmthdSXdleQjFtiSv1FDoCaxtsJTSmAbN7U7vyzEBt8",
       linkedin: 'https://www.linkedin.com/in/andrew-chen-210a72146/'
     },
-    {
-      name: 'Alex Godfrey',
-      description: 'Web',
-      picture: "https://media-exp1.licdn.com/dms/image/C5603AQEQkIucffHGSw/profile-displayphoto-shrink_200_200/0/1631155833068?e=1657756800&v=beta&t=feFMzFVFaySM3XmmiBUSmB7fK9484fyqx8P0THsLCUg",
-      linkedin: 'https://www.linkedin.com/in/alex-godfrey-91a7251b1/'
-    }
+    // {
+    //   name: 'Alex Godfrey',
+    //   description: 'Web',
+    //   picture: "https://media-exp1.licdn.com/dms/image/C5603AQEQkIucffHGSw/profile-displayphoto-shrink_200_200/0/1631155833068?e=1657756800&v=beta&t=feFMzFVFaySM3XmmiBUSmB7fK9484fyqx8P0THsLCUg",
+    //   linkedin: 'https://www.linkedin.com/in/alex-godfrey-91a7251b1/'
+    // }
   ]
 
   return (
