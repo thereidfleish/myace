e9a32c1763a2c09125aa10d83caea90587190300
Adler Weber <adlerweber8430@gmail.com>
2022-09-01 13:26:23 -0400

    Update web pics and dependencies

--- web/src/components/Home/About/Team.js
+++ web/src/components/Home/About/Team.js
@@ -5,43 +5,37 @@ export default function Team() {
     {
       name: 'Chris Price',
       description: 'Founder',
-      picture: 'https://media-exp1.licdn.com/dms/image/C5603AQGd44T4D7sVMw/profile-displayphoto-shrink_200_200/0/1614116881005?e=1657756800&v=beta&t=89Daegj4G2TcGXujGHE_5_uC0tDPejN9iWt6wnOoyd8',
+      picture: process.env.PUBLIC_URL + "/team/chris.jpg",
       linkedin: 'https://www.linkedin.com/in/christopher-price-59a24b178/'
     },
     {
       name: 'Adler Weber',
       description: 'Backend',
-      picture: "https://media-exp1.licdn.com/dms/image/C4E03AQF4q69YbpwsSQ/profile-displayphoto-shrink_200_200/0/1623430072214?e=1657756800&v=beta&t=EhJ8bCDvOiuO58y9W6k8ulCaIOy1dnvQxkNgOFrZDSc",
+      picture: process.env.PUBLIC_URL + "/team/adler.jpg",
       linkedin: 'https://www.linkedin.com/in/adler-weber-106a651ab/'
     },
-    // {
-    //   name: 'Grant Rinehimer',
-    //   description: 'Backend',
-    //   picture: "https://media.superhuman.com/images/_/https%3A%2F%2Flh6.googleusercontent.com%2Fr66PDijtSugTK_jQCYLXD_OGwI-OJW_oZRYlBPczQqMaWa0Pdh3vZHthGJJ1blwxp_JGxxzc1xs1KlUENMSoW4hlUIdyFkUue39t81i09cSobY3ZY8PnjT6DMFOrS2C7Mif9wYoCm0bh",
-    //   linkedin: 'https://www.linkedin.com'
-    // },
+    {
+      name: 'Cameron Goddard',
+      description: 'Backend',
+      picture: process.env.PUBLIC_URL + "/team/cameron.jpg",
+      linkedin: 'https://www.linkedin.com/in/cameron-goddard-ab7537222/'
+    },
     {
       name: 'Reid Fleishman',
       description: 'iOS',
-      picture: "https://media-exp1.licdn.com/dms/image/C4D03AQEO4fkuZOwEUg/profile-displayphoto-shrink_200_200/0/1641232103208?e=1657756800&v=beta&t=lMtN01HocUhvd4jsyl54tjzii7myG4ul_FlPASxLhck",
+      picture: process.env.PUBLIC_URL + "/team/reid.jpg",
       linkedin: 'https://www.linkedin.com/in/thereidfleish/'
     },
     {
       name: 'Andrew Chen',
       description: 'iOS',
-      picture: "https://media-exp1.licdn.com/dms/image/C4E03AQGkzME7hPJWvw/profile-displayphoto-shrink_200_200/0/1608691026962?e=1657756800&v=beta&t=xmthdSXdleQjFtiSv1FDoCaxtsJTSmAbN7U7vyzEBt8",
+      picture: process.env.PUBLIC_URL + "/team/andrew.jpg",
       linkedin: 'https://www.linkedin.com/in/andrew-chen-210a72146/'
     },
-    // {
-    //   name: 'Alex Godfrey',
-    //   description: 'Web',
-    //   picture: "https://media-exp1.licdn.com/dms/image/C5603AQEQkIucffHGSw/profile-displayphoto-shrink_200_200/0/1631155833068?e=1657756800&v=beta&t=feFMzFVFaySM3XmmiBUSmB7fK9484fyqx8P0THsLCUg",
-    //   linkedin: 'https://www.linkedin.com/in/alex-godfrey-91a7251b1/'
-    // }
   ]
 
   return (
-    <div className="d-flex justify-content-center flex-wrap" style={{ padding: '0px 50px'}}>
+    <div className="d-flex justify-content-center flex-wrap" style={{ padding: '0px 50px' }}>
       {
         team.map((member, i) => {
           return (
@@ -49,14 +43,14 @@ export default function Team() {
               <h4 style={{ lineHeight: '0.6' }}>{member.name}</h4>
               <h5>{member.description}</h5>
               <a target="_blank" rel="noreferrer" href={member.linkedin}>
-                <img 
-                  style={{ 
+                <img
+                  style={{
                     width: '200px',
-                    borderRadius: '50%', 
+                    borderRadius: '50%',
                     border: '3px solid #8AD28A',
                     cursor: 'pointer'
-                  }} 
-                  src={member.picture} 
+                  }}
+                  src={member.picture}
                   alt={member.name}
                 />
               </a>
--- web/src/components/Home/Home.js
+++ web/src/components/Home/Home.js
@@ -3,36 +3,32 @@ import Button from 'react-bootstrap/Button'
 import './Home.scss'
 import Timeline from './Timeline/Timeline'
 import About from './About/About'
-// import Fund from './Fund/Fund'
-import Alpha from './Alpha/Alpha'
 
 export default function Home() {
   return (
     <div>
-      <div style= {{ height: '95vh'}} className='d-flex flex-column justify-content-center'>
+      <div style={{ height: '95vh' }} className='d-flex flex-column justify-content-center'>
         <div className='d-flex flex-column align-items-center'>
           <h1 className='main-font'>
             My Ace
-            <img 
-              style={{ 
+            <img
+              style={{
                 width: "4rem",
                 paddingBottom: "0.8rem",
                 marginLeft: "1rem"
               }}
               src="https://myace.ai/logo.svg"
               alt="My Ace logo"
-              />
+            />
           </h1>
           <p className="mb-2 text-muted baby-font">tennis training on the go</p>
-          <a href="#about"><Button style={{ marginTop: '15px'}}>
-            <span style={{ color: 'white'}}>learn more →</span>
+          <a href="#about"><Button style={{ marginTop: '15px' }}>
+            <span style={{ color: 'white' }}>learn more →</span>
           </Button></a>
         </div>
       </div>
       <Timeline />
       <About />
-      {/* <Fund /> */}
-      <Alpha />
     </div>
   );
 }
